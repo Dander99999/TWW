afcde5bf46b04b4d6efcae89ed7db9ad9de13582
Dander99999 <cherednik.artm@bk.ru>
2026-06-16 10:36:14 +0300

    Delete Html and site zadanie 11 directory

--- Html and site zadanie 11/style-for-site.js
+++
@@ -1,6 +0,0 @@
-    window.addEventListener('load', function() {
-        const savedColor = localStorage.getItem('siteColor');
-        if (savedColor) {
-            document.body.style.backgroundColor = savedColor;
-        }
-    });
